bc172de17ca819ad488774cdc97f49f38cabcdc0
Patel Yash Upeshkumar <yash01298@gmail.com>
2026-06-04 08:07:25 +0530

    Refactor monitor.py for improved session handling

--- monitor.py
+++ monitor.py
@@ -7,36 +7,58 @@ CHAT_ID = os.environ["CHAT_ID"]
 
 URL = "https://support.charusat.edu.in/Uniexamresult/"
 
+
 def send(msg):
     requests.get(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    params={
-    "chat_id": CHAT_ID,
-    "text": msg[:4000]
-    }
-  )
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={
+            "chat_id": CHAT_ID,
+            "text": msg[:4000]
+        }
+    )
 
-html = requests.get(URL).text
 
+session = requests.Session()
+
+# STEP 1 - Open page
+
+html = session.get(URL).text
 soup = BeautifulSoup(html, "html.parser")
 
-inst_select = soup.find(id="ddlInst")
+viewstate = soup.find(id="__VIEWSTATE")["value"]
+generator = soup.find(id="__VIEWSTATEGENERATOR")["value"]
 
-if not inst_select:
-  send("Institution dropdown not found")
-  raise SystemExit
+# CMPICA = 2
 
-institutions = []
+payload = {
+    "__EVENTTARGET": "ddlInst",
+    "__EVENTARGUMENT": "",
+    "__VIEWSTATE": viewstate,
+    "__VIEWSTATEGENERATOR": generator,
+    "ddlInst": "2",
+    "ddlDegree": "0",
+    "txtEnrNo": ""
+}
 
-for option in inst_select.find_all("option"):
-  value = option.get("value", "").strip()
-  text = option.text.strip()
+html = session.post(URL, data=payload).text
+soup = BeautifulSoup(html, "html.parser")
 
-  if value and value != "0":
-      institutions.append(f"{value} | {text}")
+degree_select = soup.find(id="ddlDegree")
+
+if not degree_select:
+    send("Degree dropdown not found")
+    raise SystemExit
+
+degrees = []
+
+for option in degree_select.find_all("option"):
+    value = option.get("value", "").strip()
+    text = option.text.strip()
+
+    if value and value != "0":
+        degrees.append(f"{value} | {text}")
 
 send(
-"INSTITUTIONS\n\n"
-+ "\n".join(institutions)
+    "CMPICA DEGREES\n\n"
+    + "\n".join(degrees)
 )
-
